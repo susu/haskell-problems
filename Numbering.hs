01e062ce77b2f563e5498bf325c7715305265b9f
Marton Suranyi <marton.suranyi@gmail.com>
2012-08-27 20:09:49 +0200

    is_prime refactored to Numbering

--- Numbering.hs
+++ Numbering.hs
@@ -1,7 +1,13 @@
 module Numbering
 ( dividable
+, is_prime
 ) where
 
 dividable :: Int -> Int -> Bool
 dividable x y = (x `mod` y) == 0
+
+is_prime :: Int -> Bool
+is_prime 1 = False
+is_prime x = length dividers == 0
+    where dividers = (filter (dividable x) [2..floor (sqrt (fromIntegral x))])
 
--- problem3.hs
+++ problem3.hs
@@ -11,9 +11,4 @@ to = sqrt (600851475143)
 
 divisors = filter (dividable theNumber) [1..(floor to)]
 
-is_prime :: Int -> Bool
-is_prime 1 = False
-is_prime x = length dividers == 0
-    where dividers = (filter (dividable x) [2..floor (sqrt (fromIntegral x))])
-
 solution = maximum $ filter (is_prime) divisors
